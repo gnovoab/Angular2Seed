112c1eb8d02bd98148bd489fbf86c412a1c94883
gabriel.novoa <gnovoab@gmail.com>
2016-09-14 23:17:03 +0100

    Adds header

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {UserService} from "../shared/user.service";
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/components/app.component.html',
     styleUrls: ['app/components/app.component.css']
 })
-export class AppComponent {
-    pageTitle: string = 'My APP';
+export class AppComponent{
+
+    constructor(private userService: UserService) {}
+
+
 }
